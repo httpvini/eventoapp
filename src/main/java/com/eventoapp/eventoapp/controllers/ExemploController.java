e326350dbd2265e6dd8d0bb644a60f6793556b03
httpvini <vinicius.olliveira@outlook.com>
2018-01-02 02:23:25 -0200

    Primeiro Commit

---
+++ src/main/java/com/eventoapp/eventoapp/controllers/ExemploController.java
@@ -0,0 +1,16 @@
+package com.eventoapp.eventoapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exemplo")
+public class ExemploController {
+
+    @GetMapping(value = "/{nome}")
+    public String exemplo(@PathVariable("nome") String nome){
+        return "Olá "+ nome;
+    }
+}
