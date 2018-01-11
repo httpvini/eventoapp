caf29916674baea5844bbed06cf55c31225c6f24
httpvini <vinicius.olliveira@outlook.com>
2018-01-11 00:36:20 -0200

    Configuração de Profiles Dev e Prod

--- src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
+++ src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
@@ -3,6 +3,7 @@ package com.eventoapp.eventoapp;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
@@ -13,6 +14,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
+@Profile("prod")
 public class DataConfiguration {
 
     @Bean
