fb4b5ba61324a99e1c909350596e048ad5fc81cd
Vinicius Oliveira <vinicius.olliveira@outlook.com>
2020-02-04 17:23:19 -0300

    Refactoring da api rest de eventos.

--- src/main/java/com/eventoapp/adapters/cadastrorest/restapi/EventoRestController.java
+++ src/main/java/com/eventoapp/adapters/cadastrorest/restapi/EventoRestController.java
@@ -4,7 +4,9 @@ import com.eventoapp.app.models.Convidado;
 import com.eventoapp.app.models.Evento;
 import com.eventoapp.adapters.evento.repository.ConvidadoRepository;
 import com.eventoapp.adapters.evento.repository.EventoRepository;
+import com.eventoapp.app.services.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,40 +16,37 @@ import org.springframework.web.bind.annotation.*;
 public class EventoRestController {
 
     @Autowired
-    private EventoRepository er;
-
-    @Autowired
-    private ConvidadoRepository cr;
+    private EventoService eventoService;
 
     @GetMapping(value = "/evento/{codigo}")
     public ResponseEntity<Evento> consultaEvento(@PathVariable("codigo") Long codigo){
-        Evento evento = er.findByCodigo(codigo);
-        return ResponseEntity.ok().body(evento);
+        Evento evento = eventoService.buscaEvento(codigo);
+        return new ResponseEntity<>(evento, HttpStatus.OK);
     }
 
     @GetMapping(value = "/eventos")
     public ResponseEntity<?> consultaEventos(){
-        Iterable<Evento> eventos = er.findAll();
-        return ResponseEntity.ok().body(eventos);
+        Iterable<Evento> eventos = eventoService.listaEventos();
+        return new ResponseEntity<>(eventos, HttpStatus.OK);
     }
 
     @GetMapping(value = "/evento/{codigo}/convidados")
     public ResponseEntity<Iterable<Convidado>> consultaConvidados(@PathVariable("codigo") Long codigo){
-        Evento evento = er.findByCodigo(codigo);
-        Iterable<Convidado> convidados = cr.findByEvento(evento);
-        return ResponseEntity.ok().body(convidados);
+        Evento evento = eventoService.buscaEvento(codigo);
+        Iterable<Convidado> convidados = eventoService.buscaConvidados(evento);
+        return new ResponseEntity<>(convidados, HttpStatus.OK);
     }
 
     @PostMapping(value = "/evento", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Evento cadastraEvento(@RequestBody Evento evento){
-        return er.save(evento);
-
+    public ResponseEntity<?> cadastraEvento(@RequestBody Evento evento){
+        eventoService.registraEvento(evento);
+        return new ResponseEntity<>(evento, HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "/evento/{codigo}")
-    public void deletaEvento(@PathVariable Long codigo){
-        Evento evento = er.findByCodigo(codigo);
-        er.delete(evento);
+    public ResponseEntity<?> deletaEvento(@PathVariable Long codigo){
+        Evento evento = eventoService.buscaEvento(codigo);
+        eventoService.removeEvento(evento);
+        return new ResponseEntity<>(evento, HttpStatus.ACCEPTED);
     }
-
 }
